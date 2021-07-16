1817175c9f5b17328bc02198aaadf0805d1ae777
PikaGlitch <pchu20886@gmail.com>
2021-07-16 14:23:24 -0700

    lol

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="favicon.ico">
+</head>
+
+<body>
+    <div id="content">
+        <div id="scroller">
+            <?php
+                include('menu.php');
+            ?>
+            <div id="mainView">
+                <div class="holder center">
+                    <br><br><br><br><img src="MagpieDance2.gif" class="pixel"><br><br><br><br>
+                </div>
+                <div class="box center">
+                    <h2>Welcome to my Petz and Babyz site!</h2>
+                    I mostly do petz stuff, but I'll put a little babyz stuff on here too!
+                    You can use the menu on top of the page to navigate around!
+                    <br><br>
+                    <img src="cliquePink.png" class="pixel">
+                    <img src="cliqueGray.png" class="pixel">
+                    <br><br><br>
+                    <a href="https://pikaglitch.neocities.org/">Old Site</a>
+                    <br><br>
+                </div>
+
+
+
+
+
+
+            </div>
+
+        </div>
+    </div>
+
+<footer></footer>
+
+</body>
+
+</html>
---
+++ menu.php
@@ -0,0 +1,8 @@
+<div id="menuHead">
+    <div id="menu" class="center">
+        <a href = "downloads">Downloads</a>
+        <a href = "lab">The Lab</a>
+        <a href = "art">Graphics</a>
+        <a href = "crew">Crew Page</a>
+    </div>
+</div>
